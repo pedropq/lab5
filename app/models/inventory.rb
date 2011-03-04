623cf33cb071142a4978945a9195301f3ebf428a
Pedro Padilla <kalin@galileo.edu>
2011-03-03 23:52:02 -0600

    laboratorio 5

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,2 +1,6 @@
 class Inventory < ActiveRecord::Base
+   before_create {:previous_balance = 0, :new_balance = product.amount,
+   :future_balance = product.amount + product.projection,  product.amount = 0 }
+   before_update {:previous_balance = new_balance, :new_balance =  
+   product.amount, :future_balance = product.amount + product.projection }
 end
--- app/models/products.rb
+++ app/models/products.rb
@@ -1,2 +1,7 @@
 class Products < ActiveRecord::Base
+   validates_presence_of :title, :description, :price,  :image_url 
+   validates_uniqueness_of :title
+   validates_numericality_of :price, :greater_than_or_equal => 0.1
+   validates_format_of :image_url , :with => %r{\.(gif|jpg|png)$}i
+   validates_numericality_of :projection, :greater_than => 0
 end
